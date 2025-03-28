f4921bc78730cb4a89fd2c421929c19b908f849d
Ntinos <ntinosacheim@gmail.com>
2025-03-28 11:46:14 +0200

    καθαρίσαμε το φάκελο από περιττά αρχεία (Νaca και αρχεία με δεδομένα σε μοίρες). αλλαγή του json αρχείου σε πιο περιγραφικό.

--- test_algorithmos_DTU.py
+++ test_algorithmos_DTU.py
@@ -4,7 +4,7 @@ import algorithmos_DTU_10_sections as d10
 
 
 def test_one():
-    blade_geom_file_2 = "blade_geom_file_2.json"
+    blade_geom_file_2 = "blade_geom_DTU.json"
     hansen_DTU = d10.Hansen_Algorithm_for_DTU_geometry(
         blade_geom_file_2=blade_geom_file_2,
         B=3,
